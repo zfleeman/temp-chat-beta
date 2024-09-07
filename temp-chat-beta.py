70ba9628d8ee1c6a0e9f506d03e2fade8b883ef5
Zach Fleeman <zfleeman@gmail.com>
2024-09-07 14:52:00 -0600

    tidying up

--- temp-chat-beta.py
+++ temp-chat-beta.py
@@ -2,9 +2,8 @@ import discord
 import asyncio
 from datetime import datetime, timedelta
 import os
-import logging
 
-from discord.ext import tasks, commands
+from discord.ext import tasks
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,8 +13,9 @@ client = discord.Client(intents=intents)
 token = os.getenv("BOT_TOKEN")
 temp_chat_beta = int(os.getenv("CHANNEL_ID"))
 delay = int(os.getenv("BEFORE_MINUTES", default="60"))
-loop_time = int(os.getenv("SLEEP_TIME", default="30"))
-sleep_time = float(os.getenv("DELETE_SLEEP", default="0.25"))
+loop_time = int(os.getenv("LOOP_TIME", default="30"))
+sleep_time = float(os.getenv("SLEEP_TIME", default="0.25"))
+
 
 @tasks.loop(seconds=loop_time)
 async def delete_old_messages():
@@ -32,14 +32,17 @@ async def delete_old_messages():
     except Exception as e:
         print(f"An error occurred {e}")
 
+
 @delete_old_messages.before_loop
 async def startup():
     print("Waiting until the client is ready")
     await client.wait_until_ready()
 
+
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
     delete_old_messages.start()
+
 
 client.run(token=token)
